e876dcc168bd49606beb86ea622f84eed374689d
daedov <daedov@gmail.com>
2024-04-21 22:52:45 -0400

    docs: add TSDoc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * Controller responsible for handling requests related to the application.
+ */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Handles GET requests to the root endpoint.
+   * @returns A string representing the response.
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * Service class for the application.
+ */
 @Injectable()
 export class AppService {
+  /**
+   * Retrieves a greeting message.
+   * @returns The greeting message.
+   */
   getHello(): string {
     return 'Hello World!';
   }
