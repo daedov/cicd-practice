f66844258f462695e9d6726ce4637a5147c957c2
daedov <daedov@gmail.com>
2024-04-21 20:25:28 -0400

    chore(config): update ESLint and Prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,11 +15,18 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist/**', 'node_modules/**'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'linebreak-style': ["error", "unix"],
+    'prettier/prettier': [
+      'error',  
+      {
+        endOfLine: 'lf', 
+      },
+    ],
   },
 };
